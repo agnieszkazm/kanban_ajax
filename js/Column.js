b43fdce9b1503a3be5e9e47e51f159b722b3a61d
agnieszka <agnieszka.rydz@gmail.com>
2017-04-04 18:58:44 +0200

    second commit

--- js/Column.js
+++ js/Column.js
@@ -1,8 +1,7 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.name = name;
+	this.id = id;
+	this.name = name || 'Nie podano nazwy';
 	this.element = createColumn();
 
 	function createColumn() {
@@ -18,9 +17,21 @@ function Column(name) {
 			self.deleteColumn();
 		});
 		
+
 		columnAddCard.click(function(event) {
+			var cardName = prompt("Wpisz nazwę karty");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+				url: baseUrl +'/card',
+				method: 'POST',
+				data: {
+					name: cardName,
+					bootcamp_kanban_column_id: self.id
+				},
+				success: function(response) { //po poprawnym zapytaniu i utworzeniu nowej kolumny wykonuje sie:
+					var card = new Card(response.id, cardName);//tworzymy nowa karte
+					self.createCard(card);//dodajemy karte do kolumny
+			}
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
@@ -35,7 +46,17 @@ Column.prototype = {
 	createCard: function(card) {
 	  this.element.children('ul').append(card.element);
 	},
-	deleteColumn: function() {
-	  this.element.remove();
-	}
+
+//metoda delate zapytanie do serwera
+		deleteColumn: function() {
+			var self = this;
+			$.ajax ({
+				url: baseUrl + '/column/' + self.id, //$.ajax gubi kontekst
+				method: 'DELETE',
+				success: function(response){
+					self.element.remove();
+				}
+			});
+		}
+
 };
